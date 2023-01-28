21b386c534c9b8c71fe361391858f2b4347987b7
DevinLlerena23 <devinllerena@gmail.com>
2023-01-28 14:32:09 -0500

    finalizado

---
+++ src/components/Expor.js
@@ -0,0 +1,61 @@
+import React,{useState} from 'react'
+import {datos} from "./Materias";
+
+ function Materi() {
+    const [listadomaterias,setListadomaterias]=useState(datos);
+
+    console.log("Lista de Materias")
+  
+
+    const eliminar=(item)=>{
+       
+        if(window.confirm("Confirme que desea eliminar esta materia?"))
+       {
+        const nuevaLista= listadomaterias.filter(tarea=>{
+            return  tarea.Asignatura!==item.Asignatura;
+        })
+
+
+        setListadomaterias(nuevaLista);
+       }
+
+    }
+
+    return (
+        <div>
+            <div className='container'>
+                <div className='row mt-4'>
+                    {
+                        listadomaterias.map((item, index) => {
+                            return (
+                                <div className="col-md-4 col-sm-6 d-flex justify-content-center" key={index}>
+                                  <div className="card mt-4">
+                                    <div className="card header">
+                                      <h3>{item.Asignatura}</h3>
+                                      </div>
+                                      <div className="card-body d-flex flex-column align-items-center my-card-class">
+                                         <p><strong>NRC:</strong> {item.Nrc}</p> 
+                                         <p><strong>Creditos:</strong>{item.Creditos}</p>
+                                         <p><strong>Parcial 1:</strong> { item.Parcial_1}</p>
+                                         <p><strong>Parcial 2:</strong> { item.Parcial_2}</p>
+                                            
+                                            </div>
+
+                                               <div className='card-footer'>
+                                     
+                                     <p><button className='btn btn-primary' onClick={()=>eliminar(item)}>Eliminar</button></p>
+
+                                      </div>
+
+                                  </div>
+                                </div>
+                              );
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Materi;
---
+++ src/components/Materias.js
@@ -0,0 +1,52 @@
+const listamaterias=[
+
+       
+    {
+        Asignatura:"ESTRUCTURAS DISCRETAS",
+        Nrc:"6777",
+        Creditos:"3",
+        Parcial_1:"34",
+        Parcial_2:"40,75",
+    },
+        {
+            Asignatura:"CONTEXTOS E INTERCULTURALIDAD",
+            Nrc:"6603",
+            Creditos:"2",
+            Parcial_1:"42",
+            Parcial_2:"33",
+        },
+        {
+            Asignatura:"FUNDAMENTOS DE LA INVESTIGACIÓN",
+            Nrc:"6621",
+            Creditos:"2",
+            Parcial_1:"45",
+            Parcial_2:"45",
+        },
+        
+        
+        {
+            Asignatura:"SISTEMAS ELECTRÓNICOS DIGITALES",
+            Nrc:"6786",
+            Creditos:"4",
+            Parcial_1:"41,7",
+            Parcial_2:"36,78",
+        },
+       
+        
+        {
+            Asignatura:"CALCULO DIFERENCIAL E INTEGRAL",
+            Nrc:"6772",
+            Creditos:"3",
+            Parcial_1:"38,5",
+            Parcial_2:"49",
+        },
+        {
+            Asignatura:"PROGRAMACIÓN ESTRUCTURADA Y FUNCIONAL",
+            Nrc:"6782",
+            Creditos:"4",
+            Parcial_1:"32,98",
+            Parcial_2:"36,18",
+        },
+        
+];
+module.exports = { datos: listamaterias };
